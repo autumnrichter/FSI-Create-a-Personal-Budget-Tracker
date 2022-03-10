485b6359ecc719e9bdab9f8300a99e2c9b1cce50
Autumn Richter <autumn.richter@gmail.com>
2022-03-09 19:34:55 -0500

    did step 4 of exercise

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
-let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
-console.log(answer)
+for(i=0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
